a9b1fb55ab338d62af027db518b5d3eb69840228
Haoran Yang <hyang@unc.edu>
2021-08-30 20:18:20 -0400

    ex01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,9 +1,9 @@
 # TODO: Write docstring here
+""""This program is aimed to have a small conversation with you if you type in your name. """
+# TODO: Initialize __author__ variable here
+__author__: str = "730490705"
+# TODO: Implement your program logic here
 name_typed: str = input("What is your name? ")
 print(name_typed + " is a cool name! ")
 print("So, " + name_typed + ", you must be an interesting person, don't you! ")
 print("I want to be your friend, " + name_typed + "! ")
-
-# TODO: Initialize __author__ variable here
-
-# TODO: Implement your program logic here
