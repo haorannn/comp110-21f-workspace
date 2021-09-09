f1842b4e91080140d4807cf53e6786aa50b7406c
Haoran Yang <hyang@unc.edu>
2021-09-08 21:41:52 -0400

    Challenge Question 1

---
+++ exercises/Challenge_Question_1.py
@@ -0,0 +1,11 @@
+"""Challenge Question 1"""
+choice: int = int(input("Enter a number: "))
+
+if choice < 25: 
+    print("A")
+elif choice >= 25 and choice < 50: 
+    print("B")
+elif choice <= 75 and choice >= 50: 
+    print("D")
+elif choice > 75: 
+    print("C")
