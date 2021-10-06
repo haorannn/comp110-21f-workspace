36dc0ab20eab7d69c7201fb5d8d7405874f3834b
Haoran Yang <hyang@unc.edu>
2021-10-05 23:26:11 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,53 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730490705"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]: 
+    """This function returns the even numbers."""
+    even_list: list[int] = []
+    i: int = 0
+    while i < len(xs): 
+        if xs[i] % 2 == 0: 
+            even_list.append(xs[i])
+        i += 1
+    return even_list
+
+
+def sub(a_list: list[int], start_i: int, end_i: int) -> list[int]: 
+    """This function returns a part of it."""
+    sup_list: list[int] = []
+    if start_i < 0: 
+        start_i = 0
+    
+    if end_i > len(a_list): 
+        end_i = len(a_list)
+
+    if len(a_list) == 0: 
+        sup_list = []
+
+    while start_i < end_i: 
+        sup_list.append(a_list[start_i])
+        start_i += 1
+
+    return sup_list
+
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    """This function returns the combined list1 and list2."""
+    dup_list1: list[int]
+    dup_list1 = list1
+    dup_list2: list[int]
+    dup_list2 = list2
+    merge_list: list[int] = []
+    i: int = 0
+    while i < len(dup_list1): 
+        merge_list.append(dup_list1[i])
+        i += 1
+    o: int = 0
+    while o < len(dup_list2): 
+        merge_list.append(dup_list2[o])
+        o += 1
+    return merge_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,60 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730490705"
+
+
+def test_only_evens_edge_case() -> None: 
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_use_case1() -> None: 
+    xs: list[int] = [1, 2, 4, 58, 985, 211, 344]
+    assert only_evens(xs) == [2, 4, 58, 344]
+
+
+def test_only_evens_use_case2() -> None: 
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(xs) == [2, 4, 6]
+
+
+def test_sub_edge_case() -> None: 
+    xs: list[int] = []
+    beg_i: int = 9
+    end_i: int = -1
+    assert sub(xs, beg_i, end_i) == []
+
+
+def test_sub_use_case1() -> None: 
+    xs: list[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+    beg_i: int = 1
+    end_i: int = 3
+    assert sub(xs, beg_i, end_i) == [2, 3]
+
+
+def test_sub_use_case2() -> None: 
+    xs: list[int] = [1, 2, 3, 4, 5]
+    beg_i: int = 4
+    end_i: int = 5
+    assert sub(xs, beg_i, end_i) == [5]
+
+
+def test_concat_edge_case() -> None: 
+    xs1: list[int] = []
+    xs2: list[int] = []
+    assert concat(xs1, xs2) == []
+
+
+def test_concat_use_case1() -> None: 
+    xs1: list[int] = [1, 2, 3, 4, 5]
+    xs2: list[int] = [6, 7, 8, 9, 10]
+    assert concat(xs1, xs2) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_concat_use_case2() -> None: 
+    xs1: list[int] = [1]
+    xs2: list[int] = [6, 7, 8, 9, 10]
+    assert concat(xs1, xs2) == [1, 6, 7, 8, 9, 10]
